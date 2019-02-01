258c63f7aa62b0b2e2a538445c99e465f4688308
vinirajapar@gmail.com <vinirajapar@Viniras-MacBook-Pro.local>
2019-01-31 19:55:06 -0500

    changes in InventoryPage

--- src/test/java/Pages/InventoryPage.java
+++ src/test/java/Pages/InventoryPage.java
@@ -14,10 +14,10 @@ public class InventoryPage {
     @FindBy(xpath = "/html/body/div[1]/div[1]/div[1]/div[6]/div[1]/a/span")
     public WebElement Dashborad;
 
-    @FindBy(xpath = "/html/body/div[1]/div[2]/div[2]/div/div/div/div[4]/div/div[1]/div[1]/div[1]/a")
+    @FindBy(xpath = "( //span[contains(text(),'Delivery Orders')])")
     public WebElement DeliveryOrders;
 
-    @FindBy(xpath = "/html/body/div[1]/div[2]/div[2]/div/div/div/div[2]/div/div[1]/div[1]/a")
+    @FindBy(xpath = "(//span[contains(text(),'Manufacturing')])")
     public WebElement ManufacturingPage;
 
 
